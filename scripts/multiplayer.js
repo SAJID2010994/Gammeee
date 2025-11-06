7107c5f27abd5f3d6e7d8ced7a9d2b667dcdc7fd
SAJID2010994 <kamrul6666600000@gmail.com>
2025-11-06 13:47:17 +0600

    Add files via upload

--- scripts/multiplayer.js
+++ scripts/multiplayer.js
@@ -7,7 +7,22 @@ let con;
 let MyId=null
 let hostId=srch.get('id')
 if (srch.get('state')=='join') {
-	peer = new Peer()
+	peer = new Peer({
+  config: {
+    iceServers: [
+      { urls: 'stun:stun.l.google.com:19302' },
+      {
+        urls: [
+          'turn:openrelay.metered.ca:80',
+          'turn:openrelay.metered.ca:443',
+          'turn:openrelay.metered.ca:443?transport=tcp'
+        ],
+        username: 'openrelayproject',
+        credential: 'openrelayproject'
+      }
+    ]
+  }
+})
 	peer.on('open', (myid) => {
 		MyId=myid
 	con = peer.connect(hostId)
@@ -50,7 +65,22 @@ if (srch.get('state')=='join') {
 })
 }
 else if(srch.get('state')=='host'){
-	peer = new Peer()
+	peer = new Peer({
+	config: {
+		iceServers: [
+			{ urls: 'stun:stun.l.google.com:19302' },
+			{
+				urls: [
+					'turn:openrelay.metered.ca:80',
+					'turn:openrelay.metered.ca:443',
+					'turn:openrelay.metered.ca:443?transport=tcp'
+				],
+				username: 'openrelayproject',
+				credential: 'openrelayproject'
+			}
+		]
+	}
+})
 	peer.on('open',(id)=>{
 		MyId=id
 		copy(id)
